05d9c9d6ecfa077a93e8704ebf3d3ebafb59021e
Gaurav Joshi <gjoshi@calpoly.edu>
2021-01-18 23:15:10 -0800

    fixed submit button

--- sample_backend.py
+++ sample_backend.py
@@ -69,7 +69,8 @@ def get_users():
 	   userToAdd = request.get_json()
 	   userToAdd['id'] = temp
 	   users['users_list'].append(userToAdd)
-	   resp = jsonify(success=True)
+	   resp = jsonify(userToAdd)
+      #resp = jsonify(success=True)
 	   resp.status_code = 201
       #resp.status_code = 200 #optionally, you can always set a response code. 
       # 200 is the default code for a normal response	
